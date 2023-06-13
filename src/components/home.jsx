612630597ffb823d44ad815b2a452655f07cf8da
misalazeem <55094882+misalazeem@users.noreply.github.com>
2023-06-13 16:31:29 +0500

    Combined Redux and React Code

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,24 +3,10 @@ import BooksList from './BooksList';
 import AddBook from './AddBook';
 
 function Home() {
-  const BookList = [
-    {
-      id: 1,
-      title: 'Book 1',
-      author: 'Misal',
-      progress: 70,
-    },
-    {
-      id: 2,
-      title: 'Book 2',
-      author: 'Azeem',
-      progress: 80,
-    },
-  ];
   return (
     <>
       <div className="home">
-        <BooksList BookList={BookList} />
+        <BooksList />
         <AddBook />
       </div>
     </>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import booksSlice, { addbook } from './books/booksSlice';
-import categoriesSlice, { status } from './categories/categoriesSlice';
+import booksSlice from './books/booksSlice';
+import categoriesSlice from './categories/categoriesSlice';
 
 export const store = configureStore({
   reducer: {
@@ -8,13 +8,5 @@ export const store = configureStore({
     categories: categoriesSlice,
   },
 });
-
-const book1 = { title: 'Book 1', author: 'Author 1' };
-store.dispatch(addbook(book1)); // testing the addbook
-
-const book2 = { title: 'Book 2', author: 'Author 2' };
-store.dispatch(addbook(book2)); // testing the addbook
-
-store.dispatch(status()); // testing the status
 
 export default store;
